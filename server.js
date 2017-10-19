b81ab49bd6c3a45f72569fbf835cc433b5d84681
Justin Lee <leejwg.3@gmail.com>
2017-10-18 15:43:31 -1000

    server set up smoke test --> GOOD!

--- server.js
+++ server.js
@@ -1,3 +1,26 @@
 //jshint esversion:6
 
-//DB name = photodb
+const express = require('express');
+const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
+const db = require('./models');
+// const User = db.user;
+// const Gallery = db.gallery;
+const app = express();
+const PORT = process.env.PORT || 3000;
+const photosRoute = require('./routes/photos');
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.engine('.hbs', exphbs({defaultLayout: 'main', extName: '.hbs'}));
+app.set('view engine', '.hbs');
+
+app.get('/' , (req, res) =>{
+  res.json('SMOKE TESTTTTT');
+});
+
+// app.use('/photos', photosRoute);
+
+const server = app.listen(PORT, () => {
+  console.log(`Server is Listening on port: ${PORT}`);
+});
