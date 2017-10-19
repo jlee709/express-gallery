fe16e0b1baf5bd854450ecfaa2e494a3831627ce
Justin Lee <leejwg.3@gmail.com>
2017-10-18 19:52:13 -1000

    Update working, PUT route good for User edit

--- routes/photos.js
+++ routes/photos.js
@@ -6,7 +6,7 @@ const route = express.Router();
 const db = require('../models');
 const User = db.user;
 const Photo = db.photo;
-
+//home route
 route.get('/' , (req, res) =>{
   return Photo.findAll()
   .then((photos)=>{
@@ -14,6 +14,7 @@ route.get('/' , (req, res) =>{
   });
 });
 
+//show all photos from user id
 route.get('/:id' , (req, res) =>{
   const userId = req.params.id;
   return Photo.findById(userId,{
@@ -24,12 +25,14 @@ route.get('/:id' , (req, res) =>{
   });
 });
 
+//user adding photo route page 
 route.get('/:id/new' , (req, res) =>{
   //res.render page to submit new photo
   //render form with username input, text field for description
   res.json('Add New Photo page');
 });
 
+// user commiting photo page 
 route.post('/:id/new', (req, res) => {
   let userId = req.params.id;
   let title = req.body.title;
@@ -43,5 +46,28 @@ route.post('/:id/new', (req, res) => {
     return res.json(newPhoto);
   });
 });
+
+route.get('/:id/edit' , (req, res) =>{
+  //res.render page to submit new photo
+  //render form with username input, text field for description
+  res.json('Edit Photo Page');
+});
+
+
+route.put('/:id/edit', (req,res) => {
+  let userId = req.params.id;
+  let title = req.body.title;
+  let link = req.body.link;
+
+  return User.findById(userId)
+  .then((user)=>{
+    return Photo.update({title: title, link: link}, {where:{userId: user.id}})
+  .then((updatedPhoto) => {
+    return res.json(updatedPhoto);
+  });
+});
+});
+
+
 
 module.exports = route;
