8f28c1424e7c9fb0802f6fa72065ff88eb7ad08d
Ba5eem <baseem@gmail.com>
2017-10-19 13:57:59 -1000

    Added Authentication template

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,8 @@
 const express = require('express');
 const app = express();
 const route = express.Router();
+const passport = require('passport');
+const bcrypt = require('bcrypt');
 const db = require('../models');
 const User = db.user;
 const Photo = db.photo;
@@ -13,7 +15,59 @@ route.get('/' , (req, res) =>{
     return res.json(users);
   });
 });
+//REGISTER ROUTE
+app.get('/register',(req,res)=>{
+  res.render('edit');
+});
 
+app.post('/register', (req,res) =>{
+  bcrypt.genSalt(saltRounds, function(err,salt){
+    bcrypt.hash(req.body.password, salt, function(err, hash){
+      db.users.create({
+        username: req.body.username,
+        password: hash
+      })
+      .then( (user) => {
+        console.log(user);
+        res.redirect('/');
+      })
+      .catch((err) => {
+        return res.send('Stupid Username'); 
+      });
+    });
+  });
+});
+
+//SECRET ROUTE
+function isAuthenticated(req, res, next){
+  console.log(req.isAuthenticated());
+  if(req.isAuthenticated()){next();}
+  else{res.redirect('/');}
+}
+app.get('/secret', isAuthenticated, (req,res)=>{
+  console.log('req.user: ', req.user);
+  console.log('req.user id: ', req.user.id);
+  console.log('req.username: ', req.user.username);
+  console.log('req.user.password: ', req.user.password);
+  res.send('you found the secret');
+});
+
+//LOGIN ROUTE
+app.get('/login',(req,res)=>{
+  res.render('home');
+});
+
+app.post('/login', passport.authenticate('local', {
+    successRedirect: '/secret',
+    failureRedirect: '/'
+  }));
+
+app.get('/logout', (req,res) =>{
+  req.logout();
+  req.sendStatus(200);
+});
+
+//NEW ROUTE
 route.get('/new', (req, res) =>{
   //res.render page to submit new user
   //render form with username input, text field for description
@@ -35,6 +89,7 @@ route.post('/new', (req, res) => {
   });
  });
 
+//ID ROUTE
 route.get('/:id' , (req, res) =>{
   const userId = req.params.id;
   return User.findById(userId, {
--- server.js
+++ server.js
@@ -3,19 +3,75 @@
 // 167.216.15.198:3000 - baseems ip
 
 const express = require('express');
+const session = require('express-session');
 const bodyParser = require('body-parser');
 const exphbs = require('express-handlebars');
+const passport = require('passport');
+const bcrypt = require('bcrypt');
 const db = require('./models');
 const User = db.user;
 const Photo = db.photo;
 const photosRoute = require('./routes/photos');
 const usersRoute = require('./routes/users');
+const LocalStrategy = require('passport-local').Strategy;
+const saltRounds = 12;
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.engine('.hbs', exphbs({defaultLayout: 'main', extName: '.hbs'}));
 app.set('view engine', '.hbs');
+app.use(express.static('assets'));
+
+//PASSPORT - AUTHORIZATION
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user,done) => {
+  console.log("serializing");
+  return done(null, {
+    id: user.id,
+    username: user.username
+  });
+});
+
+passport.deserializeUser((user, done) => {
+  console.log('deserializing');
+  db.users.findOne({where: { id: user.id}})
+  .then(user => {
+    return done(null, {
+      id: user.id,
+      username: user.username
+    });
+  });
+});
+
+passport.use(new LocalStrategy(function(username, password, done) {
+    db.users.findOne({where: { username: username } })
+    .then( user => {
+      if(user === null) {
+        return done(null, false, {message: 'bad username or password'});
+      }
+      else { 
+        bcrypt.compare(password, user.password)
+        .then (res => {
+          console.log(res);
+          if (res) {return done(null, user); }
+          else {
+            return done(null, false, {message: 'bad username or password'});
+          }
+        });
+      }
+    })
+    .catch(err =>{
+      console.error('error: ', err);
+  });
+}));
 
 app.get('/' , (req, res) =>{
   res.json('SMOKE TESTTTTT');
