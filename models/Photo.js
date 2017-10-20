40d0691cc2138656621696bd03f7ea42a0769b3b
Ba5eem <baseem@gmail.com>
2017-10-20 08:38:53 -1000

    Updated table specs, with comments for setup

--- models/Photo.js
+++ models/Photo.js
@@ -3,7 +3,9 @@
 module.exports = function(sequelize, DataTypes){
   const Photo = sequelize.define('photo', {
     title: DataTypes.STRING,
-    link: DataTypes.STRING
+    link: DataTypes.STRING,
+    // likes: DataTypes.INTEGER - @Justin update your db table for this column:
+    //this psql syntax: ALTER TABLE photos ADD COLUMN likes integer; - then uncomment the new addition
   },{
     tableName: 'photos'
   });
